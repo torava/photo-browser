a4297fd1b1bc799a70195e396e9f940daf17974e
Teemu Orava <teemu.orava@kapsi.fi>
2026-04-11 18:49:10 +0300

    Add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,5 @@
-import Grid from '@mui/material/Grid';
-import Box from '@mui/system/Box';
-import Link from 'next/link';
+import { PhotoList } from '@/components/PhotoList/PhotoList';
 
 export default async function Home() {
-  const photos = await fetch('https://jsonplaceholder.typicode.com/photos').then((res) => res.json());
-  return (
-    <Grid container>
-      {photos.map((photo: any) => (
-        <Grid key={photo.id} size={{ xs: 12, md: 4 }}>
-          <Link href={`/photo/${photo.id}`}>
-            <Box
-              component="img"
-              src={`https://picsum.photos/1680/1050?${photo.id}`}
-              alt={photo.title}
-              loading="lazy"
-              sx={{ width: '100%', height: '100%', objectFit: 'cover' }}
-            />
-          </Link>
-        </Grid>
-      ))}
-    </Grid>
-  );
+  return <PhotoList />;
 }
---
+++ components/PhotoList/PhotoList.tsx
@@ -0,0 +1,24 @@
+import Grid from '@mui/material/Grid';
+import Box from '@mui/system/Box';
+import Link from 'next/link';
+
+export async function PhotoList() {
+  const photos = await fetch('https://jsonplaceholder.typicode.com/photos').then((res) => res.json());
+  return (
+    <Grid container>
+      {photos.map((photo: any) => (
+        <Grid key={photo.id} size={{ xs: 12, md: 4 }}>
+          <Link href={`/photo/${photo.id}`}>
+            <Box
+              component="img"
+              src={`https://picsum.photos/1680/1050?${photo.id}`}
+              alt={photo.title}
+              loading="lazy"
+              sx={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+          </Link>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
