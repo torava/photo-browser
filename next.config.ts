83912f3d2e67e8c464f7be06bed85459861edf78
Teemu Orava <teemu.orava@kapsi.fi>
2026-04-14 12:36:42 +0300

    Set output as standalone

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   distDir: 'build',
   basePath: process.env.PAGES_BASE_PATH,
 };
