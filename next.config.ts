2e2d0c93fdf852dc82db955786367749ab9cd1d1
Teemu Orava <teemu.orava@kapsi.fi>
2026-04-14 12:19:02 +0300

    Set SSG as next

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  /* config options here */
 };
 
 const withNextIntl = createNextIntlPlugin();
