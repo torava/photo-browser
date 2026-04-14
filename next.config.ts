4e4ac66b6faafeef47731f7ed5e8cfa5464bd85a
Teemu Orava <teemu.orava@kapsi.fi>
2026-04-14 12:09:36 +0300

    Add base path. Set output as export.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  distDir: 'build',
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 const withNextIntl = createNextIntlPlugin();
