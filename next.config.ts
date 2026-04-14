860758f6c3ae407d4bd9aa891f1041072d5bfbd6
Teemu Orava <teemu.orava@kapsi.fi>
2026-04-14 15:38:55 +0300

    Add test job

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/photo-browser',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 const withNextIntl = createNextIntlPlugin();
