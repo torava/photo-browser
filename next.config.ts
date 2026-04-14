12a4e42892901eee42ef468f827a87806e69259f
Teemu Orava <teemu.orava@kapsi.fi>
2026-04-14 11:41:56 +0300

    Set dist dir

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: 'build',
 };
 
 const withNextIntl = createNextIntlPlugin();
