068580cb8ccda20256e28ecbb748683b6065f00b
Teemu Orava <teemu.orava@kapsi.fi>
2026-04-14 16:10:16 +0300

    Update E2E test

--- e2e/albumId.spec.ts
+++ e2e/albumId.spec.ts
@@ -1,20 +1,7 @@
 import { expect } from '@playwright/test';
 
 import { test } from './fixtures';
-import { mockAlbum, mockAlbums, mockPhoto, mockPhotos } from './mocks';
-
-test('has title', async ({ page, mockServerRequest }) => {
-  await mockServerRequest.GET(/.*\/photos$/, {
-    body: mockPhotos,
-  });
-  await mockServerRequest.GET(/.*\/albums$/, {
-    body: mockAlbums,
-  });
-
-  await page.goto('/album?id=1');
-
-  await expect(page.getByText(`Album: ${mockAlbum.title}`)).toBeVisible();
-});
+import { mockAlbums, mockPhoto, mockPhotos } from './mocks';
 
 test('has alt text', async ({ page, mockServerRequest }) => {
   await mockServerRequest.GET(/.*\/photos$/, {
